d16bb8623a45c732b500a9e64b088809567693c0
criscamoro <camarenaocristofer@gmail.com>
2023-01-30 10:00:19 -0600

    Corrección formato rectangular
    Añadí código para generar los datos en formato rectangular y una nueva carpeta para estos datos.

--- código/code.R
+++ código/code.R
@@ -4,19 +4,19 @@
 library(tidyverse)
 library(readxl)
 
-
-#### Preparación de datos ####
-# Acomodo de los datos de forma rectangular (tidy)
+#### Preparación de datos 
+#### Tidy #### 
 
 # Datos ambientales
 env.raw <- read_excel('datos/crudos/Cajititlán_Env.xlsx', sheet = 'EnvP', skip = 1) %>% 
-  select(-'...1')
+  select(-c('...1', '...39'))
 
 write.csv(env.raw,'datos/ambiental.csv', row.names = F, na = '')
 
 # Abundancia de zooplancton
 
-zoo.raw <- read_excel('datos/crudos/Cajititlán_Bio.xlsx', sheet = 'ZooP', col_names = F, skip = 1) %>% 
+zoo.raw <- read_excel('datos/crudos/Cajititlán_Bio.xlsx', 
+                      sheet = 'ZooP', col_names = F, skip = 1) %>% 
   slice(-c(1,8)) %>%
   column_to_rownames('...1') %>%
   t
@@ -33,8 +33,21 @@ fito.raw <- read_excel('datos/crudos/Cajititlán_Bio.xlsx', sheet = 'FitoP', ski
 
 write.csv(fito.raw, 'datos/fitoplancton.csv', row.names = F, na = '0')
 
+#### Rectangular ####
+# Ambientales
+env.rect <- read_csv('datos/ambiental.csv') %>% 
+  pivot_longer(cols = c(1:37), names_to = 'var', values_to = 'value')
 
+write.csv(env.rect, 'datos/rectangulares/ambiental_rect.csv')
 
+# Zooplancton
+zoo.rect <- read_csv('datos/zooplancton.csv') %>% 
+  pivot_longer(cols = c(1:6), names_to = 'taxa', values_to = 'count')
 
+write.csv(zoo.rect, 'datos/rectangulares/zooplancton_rect.csv')
 
+# Fitoplancton
+fito.rect <- read_csv('datos/fitoplancton.csv') %>% 
+  pivot_longer(cols = c(1:60), names_to = 'taxa', values_to = 'count')
 
+write.csv(fito.rect, 'datos/rectangulares/fitoplancton_rect.csv')
